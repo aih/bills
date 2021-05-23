bdc97c97c0326f3e8f5f2f6cb55c4daca95ccbe9
Ari Hershowitz <arihershowitz@gmail.com>
2021-05-23 11:25:48 -0700

    Basic query is working

--- elasticsimilarity.go
+++ elasticsimilarity.go
@@ -1,69 +1,96 @@
 package bills
 
 import (
-	"log"
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/rs/zerolog/log"
 
 	"github.com/elastic/go-elasticsearch/v7"
 )
 
 var (
-	getAllQuery = `{"query": {"match_all" : {}},"size": 2}`
+	r             map[string]interface{}
+	searchIndices = []string{"billsections"}
 )
 
-func printESInfo() {
-	log.SetFlags(0)
+func PrintESInfo() {
 	es, err := elasticsearch.NewDefaultClient()
 	if err != nil {
-		log.Fatalf("Error creating the client: %s", err)
+		log.Fatal().Msgf("Error creating the client: %s", err)
 	}
 	res, err := es.Info()
 	if err != nil {
-		log.Fatalf("Error getting response: %s", err)
+		log.Fatal().Msgf("Error getting response: %s", err)
 	} else {
-		log.Println(res)
-		log.Println(elasticsearch.Version)
+		log.Info().Msg(fmt.Sprint(res))
+		log.Info().Msg(fmt.Sprint(elasticsearch.Version))
 	}
 }
 
-/*
-func getAllDocs() {
-	log.SetFlags(0)
+func SampleQuery() {
 	es, err := elasticsearch.NewDefaultClient()
 	if err != nil {
-		log.Fatalf("Error creating the client: %s", err)
+		log.Fatal().Msgf("Error creating the client: %s", err)
 	}
 
-	// Set up the request object.
-	req := esapi.IndexRequest{
-		Index:      "billsections",
-		DocumentID: "115hconres76ih",
-		Body:       strings.NewReader(getAllQuery),
-		Refresh:    "true",
+	// 3. Search for the indexed documents
+	//
+	// Build the request body.
+	var buf bytes.Buffer
+	query := map[string]interface{}{
+		"query": map[string]interface{}{
+			"match": map[string]interface{}{
+				"billnumber": "115hr4134",
+			},
+		},
+	}
+	if err := json.NewEncoder(&buf).Encode(query); err != nil {
+		log.Fatal().Msgf("Error encoding query: %s", err)
 	}
 
-	// Perform the request with the client.
-	_, err = req.Do(context.Background(), es)
+	// Perform the search request.
+	res, err := es.Search(
+		es.Search.WithContext(context.Background()),
+		es.Search.WithIndex(searchIndices...),
+		es.Search.WithBody(&buf),
+		es.Search.WithTrackTotalHits(true),
+		es.Search.WithPretty(),
+	)
 	if err != nil {
-		log.Fatalf("Error getting response: %s", err)
+		log.Fatal().Msgf("Error getting response: %s", err)
+	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		var e map[string]interface{}
+		if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
+			log.Fatal().Msgf("Error parsing the response body: %s", err)
+		} else {
+			// Print the response status and error information.
+			log.Fatal().Msgf("[%s] %s: %s",
+				res.Status(),
+				e["error"].(map[string]interface{})["type"],
+				e["error"].(map[string]interface{})["reason"],
+			)
+		}
 	}
 
-	docs := 0
-	for {
-		res, err := scroller.Do(context.TODO())
-		if err == io.EOF {
-			// No remaining documents matching the search so break out of the 'forever' loop
-			break
-		}
-		for _, hit := range res.Hits.Hits {
-			// JSON parse or do whatever with each document retrieved from your index
-			item := make(map[string]interface{})
-			err := json.Unmarshal(*hit.Source, &item)
-			if err != nil {
-				log.Err().Err(err)
-			}
-			docs++
-		}
+	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+		log.Fatal().Msgf("Error parsing the response body: %s", err)
+	}
+	// Print the response status, number of results, and request duration.
+	fmt.Printf(
+		"[%s] %d hits; took: %dms",
+		res.Status(),
+		int(r["hits"].(map[string]interface{})["total"].(map[string]interface{})["value"].(float64)),
+		int(r["took"].(float64)),
+	)
+	// Print the ID and document source for each hit.
+	for _, hit := range r["hits"].(map[string]interface{})["hits"].([]interface{}) {
+		fmt.Printf(" * ID=%s, %s", hit.(map[string]interface{})["_id"], hit.(map[string]interface{})["_source"])
 	}
 
 }
-*/
