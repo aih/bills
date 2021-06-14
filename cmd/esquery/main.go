690f1519f336e02a04f68b2f85fb885b8812714f
Ari Hershowitz <arihershowitz@gmail.com>
2021-06-13 23:36:40 -0700

    Similarity query working

--- cmd/esquery/main.go
+++ cmd/esquery/main.go
@@ -27,8 +27,26 @@ func main() {
 	//bills.PrintESInfo()
 	//bills.SampleQuery()
 
-	r := bills.BillQuery("116hr1500")
-	bills.GetLatestBill(r)
-	// billNumbers := bills.GetAllBillNumbers()
+	billNumbers := bills.GetAllBillNumbers()
+	billNumbers = []string{"116hr1500"}
+	for _, billnumber := range billNumbers {
+		log.Info().Msgf("Get versions of: %s", billnumber)
+		r := bills.GetBill_ES(billnumber)
+		latestbill := bills.GetLatestBill(r)
+		billversion := latestbill["_source"].(map[string]interface{})["billversion"].(string)
+		billnumberversion := billnumber + billversion
+		billsections := latestbill["_source"].(map[string]interface{})["sections"].([]interface{})
+		log.Info().Msgf("Get similar bills for the %d sections of %s", len(billsections), billnumberversion)
+		for _, sectionItem := range billsections {
+			//sectionHeader := sectionItem.(map[string]interface{})["section_header"]
+			//sectionNumber := sectionItem.(map[string]interface{})["section_number"]
+			sectionText := sectionItem.(map[string]interface{})["section_text"]
+			similars := bills.GetMoreLikeThisQuery(5, 20, sectionText.(string))
+			hits := similars["hits"].(map[string]interface{})["hits"].([]interface{})
+			if len(hits) > 0 {
+				log.Info().Msgf("Number of matches: %d, Score (first match): %f", len(hits), hits[0].(map[string]interface{})["_score"])
+			}
+		}
+	}
 	// fmt.Print(billNumbers)
 }
--- elasticsimilarity.go
+++ elasticsimilarity.go
@@ -17,11 +17,49 @@ import (
 )
 
 var (
-	r             map[string]interface{}
-	batchNum      int
-	scrollID      string
-	searchIndices = []string{"billsections"}
+	batchNum          int
+	scrollID          string
+	searchIndices     = []string{"billsections"}
+	moreLikeThisQuery = map[string]interface{}{
+		"size":      5,
+		"min_score": 15,
+		"query": map[string]interface{}{
+			"nested": map[string]interface{}{
+				"path": "sections",
+				"query": map[string]interface{}{
+					"more_like_this": map[string]interface{}{
+						"fields":          []string{"sections.section_text"},
+						"like":            `SEC. 102. COLORADO WILDERNESS ADDITIONS. (a) Designation.—Section 2(a) of the Colorado Wilderness Act of 1993 (16 U.S.C. 1132 note; Public Law 103–77) is amended— (1) in paragraph (18), by striking “1993,” and inserting “1993, and certain Federal land within the White River National Forest that comprises approximately 6,896 acres, as generally depicted as ‘Proposed Ptarmigan Peak Wilderness Additions’ on the map entitled ‘Proposed Ptarmigan Peak Wilderness Additions’ and dated June 24, 2019,”; and (2) by adding at the end the following:`,
+						"min_term_freq":   2,
+						"max_query_terms": 10,
+						"min_doc_freq":    1,
+					},
+				},
+				"inner_hits": map[string]interface{}{
+					"highlight": map[string]interface{}{
+						"fields": map[string]interface{}{
+							"sections.section_text": map[string]interface{}{},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	idQuery = map[string]interface{}{
+		"query": map[string]interface{}{
+			"match_all": map[string]interface{}{},
+		},
+		"fields":  []string{"id"},
+		"_source": false,
+	}
 )
+
+func read(r io.Reader) string {
+	var b bytes.Buffer
+	b.ReadFrom(r)
+	return b.String()
+}
 
 func PrintESInfo() {
 	es, err := elasticsearch.NewDefaultClient()
@@ -37,44 +75,18 @@ func PrintESInfo() {
 	}
 }
 
-func GetLatestBill(r map[string]interface{}) (latestbill map[string]interface{}) {
-	latestdate, _ := time.Parse(time.RFC3339, time.RFC3339)
-	latestbillversion := "ih"
-	latestbillversion_val := 0
-	for _, hit := range r["hits"].(map[string]interface{})["hits"].([]interface{}) {
-		billversion := hit.(map[string]interface{})["_source"].(map[string]interface{})["billversion"].(string)
-		datestring := hit.(map[string]interface{})["_source"].(map[string]interface{})["date"]
-		if datestring == nil {
-			datestring = ""
-		}
-		if datestring != "" {
-			date, err := time.Parse(time.RFC3339, datestring.(string)+"T15:04:05Z")
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			// Use the date if the latest version is not an "e" version
-			if date.After(latestdate) && !strings.HasPrefix(latestbillversion, "e") {
-				latestdate = date
-				latestbillversion = billversion
-				latestbillversion_val = BillVersionsOrdered[latestbillversion]
-				latestbill = hit.(map[string]interface{})
-			}
-		}
-		if billversion_val, ok := BillVersionsOrdered[billversion]; ok {
-			if strings.HasPrefix(billversion, "e") && (billversion_val > latestbillversion_val) {
-				fmt.Println("now here")
-				latestbillversion = billversion
-				latestbill = hit.(map[string]interface{})
-			}
-		}
-		log.Debug().Msgf("bill=%s; date=%s", billversion, datestring)
+func makeBillQuery(billnumber string) (billquery map[string]interface{}) {
+	billquery = map[string]interface{}{
+		"query": map[string]interface{}{
+			"match": map[string]interface{}{
+				"billnumber": billnumber,
+			},
+		},
 	}
-	log.Debug().Msgf("latestbillversion=%s; latestdate=%s", latestbillversion, latestdate.String())
-	return latestbill
+	return billquery
 }
 
-func BillQuery(billnumber string) map[string]interface{} {
+func runQuery(query map[string]interface{}) (r map[string]interface{}) {
 	es, err := elasticsearch.NewDefaultClient()
 	if err != nil {
 		log.Fatal().Msgf("Error creating the client: %s", err)
@@ -83,13 +95,7 @@ func BillQuery(billnumber string) map[string]interface{} {
 	// Search for the indexed documents
 	// Build the request body.
 	var buf bytes.Buffer
-	query := map[string]interface{}{
-		"query": map[string]interface{}{
-			"match": map[string]interface{}{
-				"billnumber": billnumber,
-			},
-		},
-	}
+
 	if err := json.NewEncoder(&buf).Encode(query); err != nil {
 		log.Fatal().Msgf("Error encoding query: %s", err)
 	}
@@ -124,10 +130,14 @@ func BillQuery(billnumber string) map[string]interface{} {
 	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
 		log.Fatal().Msgf("Error parsing the response body: %s", err)
 	}
+
+	if res.Status() != "200 OK" {
+		log.Error().Msgf("ES search: [%s]", res.Status())
+	}
+
 	// Print the response status, number of results, and request duration.
-	log.Info().Msgf(
-		"ES bill search for %s: [%s] %d hits; took %dms",
-		billnumber,
+	log.Debug().Msgf(
+		"ES search: [%s] %d hits; took %dms",
 		res.Status(),
 		int(r["hits"].(map[string]interface{})["total"].(map[string]interface{})["value"].(float64)),
 		int(r["took"].(float64)),
@@ -137,30 +147,22 @@ func BillQuery(billnumber string) map[string]interface{} {
 		log.Debug().Msgf(" * ID=%s, %s", hit.(map[string]interface{})["_id"], hit.(map[string]interface{})["_source"])
 	}
 	return r
-
 }
 
-func getIdQuery() bytes.Buffer {
-
-	// Search indexed documents with a `match_all` query to retrieve all
-	// Build the request body.
-	var buf bytes.Buffer
-	query := map[string]interface{}{
-		"query": map[string]interface{}{
-			"match_all": map[string]interface{}{},
-		},
-		"fields":  []string{"id"},
-		"_source": false,
-	}
-	if err := json.NewEncoder(&buf).Encode(query); err != nil {
-		log.Fatal().Msgf("Error encoding query: %s", err)
-	}
-	return buf
-
+func GetBill_ES(billnumber string) map[string]interface{} {
+	r := runQuery(makeBillQuery(billnumber))
+	return r
 }
 
-// Performs scroll query over indices in `searchIndices`; sends result to the resultChan for processing
-func scrollQuery(buf bytes.Buffer, resultChan chan []gjson.Result) {
+func GetMoreLikeThisQuery(size, minscore int, searchtext string) map[string]interface{} {
+	moreLikeThisQuery["size"] = size
+	moreLikeThisQuery["min_score"] = minscore
+	moreLikeThisQuery["query"].(map[string]interface{})["nested"].(map[string]interface{})["query"].(map[string]interface{})["more_like_this"].(map[string]interface{})["like"] = searchtext
+	return runQuery(moreLikeThisQuery)
+}
+
+// Performs scroll query over indices in `searchIndices`; sends result to the resultChan for processing to extract billnumbers
+func scrollQueryBillNumbers(buf bytes.Buffer, resultChan chan []gjson.Result) {
 	defer close(resultChan)
 	es, err := elasticsearch.NewDefaultClient()
 	if err != nil {
@@ -172,7 +174,6 @@ func scrollQuery(buf bytes.Buffer, resultChan chan []gjson.Result) {
 	//
 	log.Info().Msg("Scrolling the index...")
 	log.Info().Msg(strings.Repeat("-", 80))
-	//buf := queryFunc(es)
 	res, _ := es.Search(
 		es.Search.WithIndex(searchIndices...),
 		es.Search.WithBody(&buf),
@@ -238,17 +239,59 @@ func scrollQuery(buf bytes.Buffer, resultChan chan []gjson.Result) {
 	}
 }
 
+// Sort the eh, es, and enr as latest
+// Then sort by date
+// TODO: better method is to get the latest version in Fdsys_billstatus
+func GetLatestBill(r map[string]interface{}) (latestbill map[string]interface{}) {
+	latestdate, _ := time.Parse(time.RFC3339, time.RFC3339)
+	latestbillversion := "ih"
+	latestbillversion_val := 0
+	for _, hit := range r["hits"].(map[string]interface{})["hits"].([]interface{}) {
+		billversion := hit.(map[string]interface{})["_source"].(map[string]interface{})["billversion"].(string)
+		datestring := hit.(map[string]interface{})["_source"].(map[string]interface{})["date"]
+		if datestring == nil {
+			datestring = ""
+		}
+		if datestring != "" {
+			date, err := time.Parse(time.RFC3339, datestring.(string)+"T15:04:05Z")
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			// Use the date if the latest version is not an "e" version
+			if date.After(latestdate) && !strings.HasPrefix(latestbillversion, "e") {
+				latestdate = date
+				latestbillversion = billversion
+				latestbillversion_val = BillVersionsOrdered[latestbillversion]
+				latestbill = hit.(map[string]interface{})
+			}
+		}
+		if billversion_val, ok := BillVersionsOrdered[billversion]; ok {
+			if strings.HasPrefix(billversion, "e") && (billversion_val > latestbillversion_val) {
+				latestbillversion = billversion
+				latestbill = hit.(map[string]interface{})
+			}
+		}
+		log.Debug().Msgf("bill=%s; date=%s", billversion, datestring)
+	}
+	log.Debug().Msgf("latestbillversion=%s; latestdate=%s", latestbillversion, latestdate.String())
+	return latestbill
+}
+
 func GetAllBillNumbers() []string {
 	var billNumbers []gjson.Result
 	resultChan := make(chan []gjson.Result)
-	buf := getIdQuery()
-	go scrollQuery(buf, resultChan)
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(idQuery); err != nil {
+		log.Fatal().Msgf("Error encoding query: %s", err)
+	}
+	go scrollQueryBillNumbers(buf, resultChan)
 	for newBillNumbers := range resultChan {
 		billNumbers = append(billNumbers, newBillNumbers...)
 	}
 	//fmt.Println(billNumbers)
 	// billNumbers is an Array of gjson.Result;
-	// each result is itself an array of string of the formo
+	// each result is itself an array of string of the form
 	//["117hr141ih"]
 	log.Info().Msgf("Length of billNumbers: %d", len(billNumbers))
 	var billNumberStrings []string
@@ -262,10 +305,4 @@ func GetAllBillNumbers() []string {
 		}
 	}
 	return billNumberStrings
-}
-
-func read(r io.Reader) string {
-	var b bytes.Buffer
-	b.ReadFrom(r)
-	return b.String()
 }
